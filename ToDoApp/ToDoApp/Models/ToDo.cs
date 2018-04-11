0fa62f8bcc026456d353edee7e5a085579444cc4
Gerda Csuzdi <moira85@gmail.com>
2018-04-11 14:03:02 +0200

    init proj, add todo class

---
+++ ToDoApp/ToDoApp/Models/ToDo.cs
@@ -0,0 +1,17 @@
+﻿namespace ToDoApp.Models
+{
+    public class ToDo
+    {
+        private long id;
+        private string title;
+        private bool isUrgent;
+        private bool isDone;
+
+        public ToDo(string title, bool isUrgent)
+        {
+            this.title = title;
+            this.isUrgent = isUrgent;
+            this.isDone = false;
+        }
+    }
+}
