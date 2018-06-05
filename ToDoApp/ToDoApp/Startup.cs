b03fb453a1d9aa1e2b3db7292ff033fde470132b
Gerda Csuzdi <moira85@gmail.com>
2018-06-05 17:03:05 +0200

    delete comments

--- ToDoApp/ToDoApp/Startup.cs
+++ ToDoApp/ToDoApp/Startup.cs
@@ -4,24 +4,19 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using Microsoft.EntityFrameworkCore.Storage;
 using ToDoApp.Repositories;
 
 namespace ToDoApp
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-            //services.AddDbContext<ToDoContext>(options => options.UseSqlServer(Environment.GetEnvironmentVariable("SQL_CONNECTIONSTRING")));
             services.AddDbContext<ToDoContext>(options => options.UseNpgsql(Environment.GetEnvironmentVariable("NPG_CONNECTIONSTRING")));
             services.AddScoped<IDoThingsRepo, ToDoRepo>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
