b65770e446c2462b5669d816699df89826743656
Gerda Csuzdi <moira85@gmail.com>
2018-06-12 14:11:11 +0200

    add service layer, with ICrudRepository interface

---
+++ ToDoApp/ToDoApp/Repositories/ICrudRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Services
+{
+    public interface ICrudRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync(IQueryCollection q);
+        Task CreateAsync(T entity);
+        Task<T> GettByIdAsync(long id);
+        Task UpdateAsync(T entity);
+        Task DeleteByIdAsync(long id);
+
+        Task LoadContextAsync();
+    }
+}
+
---
+++ ToDoApp/ToDoApp/Services/ToDoServices.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using ToDoApp.Models;
+
+namespace ToDoApp.Services
+{
+    public class ToDoServices : ICrudRepository<ToDo>
+    {
+        public Task CreateAsync(ToDo entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteByIdAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ToDo>> GetAllAsync(IQueryCollection q)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ToDo> GettByIdAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task LoadContextAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(ToDo entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
